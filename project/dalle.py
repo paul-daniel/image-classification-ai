8f7dc815448db3465f9a4c3036c83ec8e52979c9
Paul Daniel <dantchatchouang@gmail.com>
2026-04-30 06:43:40 +0200

    wip: init files content

---
+++ project/dalle.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from dalle-project!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ project/vision.py
@@ -0,0 +1,5 @@
+def main():
+    print("Hello from vision-project!")
+
+if __name__ == "__main__":
+    main()
